4482aeb2698408f66466ad0f1347588a92e33d19
VAIBHAVI-17 <98948786+VAIBHAVI-17@users.noreply.github.com>
2022-12-01 00:37:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 st.title("TDS Week 8 Module")
 a=st.number_input("Enter first number")
-b=st.number_input("Enter second number", value=1)
+b=st.number_input("Enter second number", value=1.00)
 try:
   c=a/b
 except ZeroDivisionError:
-  st.write("Dic=vision by zero is not posible")
+  st.write("Division by zero is not posible")
 else:
   st.write("Division answer is",c)
+st.write("By Vaibhavi Tiwari 21f2000087")
